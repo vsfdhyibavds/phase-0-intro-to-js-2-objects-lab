eaffacdf1e543c20dfbe5d1be05ea48ed2979a63
Eugene Wekesa <eugenco578@gmail.com>
2025-03-20 07:05:35 +0300

    practiced creating an Object and performing operations on it

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+    streetAddress : '456 Broadway',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+    }
+
+    function deleteFromEmployeeByKey(employee, key, value) {
+        const {[key] : deletekey, ...newWmployee} = employee
+        return newWmployee;
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employee, keyy, value) {
+        delete employee[keyy];
+        return employee;
+    }
